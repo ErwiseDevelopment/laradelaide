34cad981aecfb60000fc32fb6988e6276503618e
Fabio de Melo <fabiomessias98@outlook.com>
2022-07-22 14:05:14 -0300

    Adicionado o erwise no rodape

--- wp-content/themes/wp-bootstrap-starter-child/footer.php
+++ wp-content/themes/wp-bootstrap-starter-child/footer.php
@@ -135,6 +135,14 @@
                     </div>
                 </div>
             </div>
+
+            <div class="container">
+
+                <div class="row">
+
+                    <?php echo get_template_part( 'template-parts/content', 'dev' ) ?>
+                </div>
+            </div>
         </div>
     </footer>
 
